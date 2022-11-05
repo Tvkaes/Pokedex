66aaa81d9d2aeadab002ee1b7a99b4c52fb2a90a
tvkaes <dertvk8s@gmail.com>
2022-11-05 14:37:36 -0700

    next js config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images:{
-    domains:['raw.githubusercontent.com']
+    domains:['raw.githubusercontent.com','static.wikia.nocookie.net']
   }
 }
 
